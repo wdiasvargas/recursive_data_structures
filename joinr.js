1e829354e9df097e5027578b2f9efb537f17c5a7
William Dias <williamdiasvargas@gmail.com>
2019-02-28 20:58:38 -0300

    First commit
    recursive functions for data structures

--- joinr.js
+++ joinr.js
@@ -1,5 +1,5 @@
-const joinr = ([head, ...tail], separator = ',') => (head === undefined && !tail.length) 
-    ? '' 
-    : tail.length 
-        ? head + separator + joinr(tail, separator) 
-        : head
+const joinr = ([head, ...tail], separator = '-') => (head === undefined && !tail.length) ?
+  '' :
+  tail.length ?
+  head + separator + joinr(tail, separator) :
+  head
--- reducer.js
+++ reducer.js
@@ -1,4 +1,5 @@
-const reducer = (fn, acc, [head, ...tail]) =>
-  head === undefined 
-  ? acc 
-  : reducer(fn, fn(acc, head), tail)
+const reducer = (fn, acc, [head, ...tail]) => head === undefined ?
+  acc : reducer(fn, fn(acc, head), tail)
+
+const double = x => x * 3
+console.log(reducer(double,0,[1,2]))
